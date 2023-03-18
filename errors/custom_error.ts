696290b659a0ce148c12824a30e456c8cfb9a1bd
Subhasish Biswasray <subhasish.biswasray@bipros.com>
2023-03-19 01:03:25 +0530

    docs(errors): wrote description for error classes

--- errors/custom_error.ts
+++ errors/custom_error.ts
@@ -1,13 +1,28 @@
 import { IErrorSerialized } from '../interfaces/errors';
 
+/**
+ * CustomError
+ * 
+ * @alias CustomError
+ */
 export class CustomError<T extends string[] | Record<string, string>> extends Error {
   statusCode: number = 0;
   errors: T | undefined;
+  /**
+   * @summary create CustomError object to throw.
+   * - Example: `throw new CustomError(402, ["Something is wrong!"]);`
+   * @param statusCode {Number} custom status code.
+   * @param errors {Object} array or object of messages for CustomError.
+   */
   constructor(statusCode: number, errors: T) {
     super(Object.values(errors).join(' ') || 'Custom error occurred');
     this.statusCode = statusCode;
     this.errors = errors;
   }
+  /**
+   * Serialize `CustomError` to JSON format for server response 
+   * @returns {Object} JSON object of error messages
+   */
   serialize(): IErrorSerialized[] {
     const result: IErrorSerialized[] = [];
     for (const i in this.errors) result.push({ message: this.errors[i] } as IErrorSerialized);
--- errors/platform_error.ts
+++ errors/platform_error.ts
@@ -1,7 +1,18 @@
 import { StatusCodes } from '../utils/status_codes';
 import { CustomError } from './custom_error';
 
+/**
+ * PlatformError
+ * 
+ * @alias PlatformError
+ */
 export class PlatformError extends CustomError<string[]> {
+  /**
+   * @summary create PlatformError object to throw.
+   * - Example: `throw new PlatformError("Not Found", { messages: "Page not found", resource: "/user" });`
+   * @param status {String} type of HTTP response status
+   * @param options {Object} extra meassages or resource related to error
+   */
   constructor(status: keyof typeof StatusCodes, options?: { messages?: string[]; resource?: string }) {
     const messages: string[] = [status, ...(options?.messages ?? [])];
     if (options?.resource) {
