22252a12ac50d1a1c754a4d5007149999f3d6ad3
Subhasish Biswasray <subhasish9583672357@gmail.com>
2023-05-08 11:47:52 +0530

    feat(errors): added Platform Error options message single string type

--- errors/platform_error.ts
+++ errors/platform_error.ts
@@ -10,14 +10,19 @@ export class PlatformError extends CustomError<string[]> {
   /**
    * @summary create PlatformError object to throw.
    * - Example: `throw new PlatformError("Not Found", { messages: "Page not found", resource: "/user" });`
-   * @param status {String} type of HTTP response status -- 
+   * @param status {String} type of HTTP response status --
    * 'Bad Request' | 'Unauthorized' | 'Payment Required' | 'Forbidden' | 'Not Found' | 'Proxy Authentication Required' | 'Request Timeout' | 'Internal Server Error' | 'Not Implemented' | 'Bad Gateway' | 'Service Unavailable'.
    * Explore more status codes https://github.com/biswasray/platform-error/blob/master/utils/status_codes.ts
    * @param options {Object} extra meassages or resource related to error
    * `{ messages?: string[]; resource?: string }`
    */
-  constructor(status: keyof typeof StatusCodes, options?: { messages?: string[]; resource?: string }) {
-    const messages: string[] = [status, ...(options?.messages ?? [])];
+  constructor(status: keyof typeof StatusCodes, options?: { messages?: string | string[]; resource?: string }) {
+    let messages: string[] = [];
+    if (typeof options?.messages === 'string') {
+      messages = [status, options?.messages];
+    } else {
+      messages = [status, ...(options?.messages ?? [])];
+    }
     if (options?.resource) {
       messages.push(options.resource);
     }
